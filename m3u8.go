f81d4e913b448cca6187ce31b76e9cfb7510be45
LinWenBin <51529210@qq.com>
2021-02-12 00:03:48 +0800

    备份

--- m3u8.go
+++ m3u8.go
@@ -1,11 +1,129 @@
+// 参考文档：
+// https://tools.ietf.org/html/rfc8216
 package m3u8
+
+import "io"
 
 const (
 	ContentType = "application/vnd.apple.mpegurl"
+	// basic tags
+	TagEXTM3U        = "#EXTM3U"
+	TagEXT_X_VERSION = "#EXT-X-VERSION"
+	// media segment tags
+	TagEXTINF                  = "#EXTINF"
+	TagEXT_X_BYTERANGE         = "#EXT-X-BYTERANGE"
+	TagEXT_X_DISCONTINUITY     = "#EXT-X-DISCONTINUITY"
+	TagEXT_X_KEY               = "#EXT-X-KEY"
+	TagEXT_X_MAP               = "#EXT-X-MAP"
+	TagEXT_X_PROGRAM_DATE_TIME = "#EXT-X-PROGRAM-DATE-TIME"
+	TagEXT_X_DATERANGE         = "#EXT-X-DATERANGE"
+	// media playlist tags
+	TagEXT_X_TARGETDURATION         = "#EXT-X-TARGETDURATION"
+	TagEXT_X_MEDIA_SEQUENCE         = "#EXT-X-MEDIA-SEQUENCE"
+	TagEXT_X_DISCONTINUITY_SEQUENCE = "#EXT-X-DISCONTINUITY-SEQUENCE"
+	TagEXT_X_ENDLIST                = "#EXT-X-ENDLIST"
+	TagEXT_X_PLAYLIST_TYPE          = "#EXT-X-PLAYLIST-TYPE"
+	TagEXT_X_I_FRAMES_ONLY          = "#EXT-X-I-FRAMES-ONLY"
+	// master playlist tags
+	TagEXT_X_MEDIA                = "#EXT-X-MEDIA"
+	TagEXT_X_STREAM_INF           = "#EXT-X-STREAM-INF"
+	TagEXT_X_I_FRAME_STREAM_INF   = "#EXT-X-I-FRAME-STREAM-INF"
+	TagEXT_X_SESSION_DATA         = "#EXT-X-SESSION-DATA"
+	TagEXT_X_SESSION_KEY          = "#EXT-X-SESSION-KEY"
+	TagEXT_X_INDEPENDENT_SEGMENTS = "#EXT-X-INDEPENDENT-SEGMENTS"
+	TagEXT_X_START                = "#EXT-X-START"
 )
 
+type EXTINF struct {
+	DURATION string
+	TITLE    string
+}
+
+type EXT_X_BYTERANGE struct {
+	N string
+	O string
+}
+
+type EXT_X_KEY struct {
+	METHOD            string
+	URI               string
+	IV                string
+	KEYFORMAT         string
+	KEYFORMATVERSIONS string
+}
+
+type EXT_X_MAP struct {
+	URI       string
+	BYTERANGE string
+}
+
+type EXT_X_DATERANGE struct {
+	ID                 string
+	CLASS              string
+	START_DATE         string
+	END_DATE           string
+	DURATION           string
+	PLANNED_DURATION   string
+	X_CLIENT_ATTRIBUTE map[string]string
+	SCTE35_CMD         string
+	SCTE35_OUT         string
+	SCTE35_IN          string
+	END_ON_NEXT        string
+}
+
+type EXT_X_MEDIA struct {
+	TYPE            string
+	URI             string
+	GROUP_ID        string
+	LANGUAGE        string
+	ASSOC_LANGUAGE  string
+	NAME            string
+	DEFAULT         string
+	AUTOSELECT      string
+	FORCED          string
+	INSTREAM_ID     string
+	CHARACTERISTICS string
+	CHANNELS        string
+}
+
+type EXT_X_STREAM_INF struct {
+	BANDWIDTH         string
+	AVERAGE_BANDWIDTH string
+	CODECS            string
+	RESOLUTION        string
+	FRAME_RATE        string
+	HDCP_LEVEL        string
+	AUDIO             string
+	VIDEO             string
+	SUBTITLES         string
+	CLOSED_CAPTIONS   string
+	URI               string
+}
+
+type EXT_X_I_FRAME_STREAM_INF struct {
+	BANDWIDTH         string
+	AVERAGE_BANDWIDTH string
+	CODECS            string
+	RESOLUTION        string
+	HDCP_LEVEL        string
+	VIDEO             string
+	URI               string
+}
+
+type EXT_X_SESSION_DATA struct {
+	DATA_ID  string
+	VALUE    string
+	URI      string
+	LANGUAGE string
+}
+
+type EXT_X_START struct {
+	TIME_OFFSET string
+	PRECISE     string
+}
+
 type MediaSegment struct {
-	EXTINF                  *EXTINF
+	EXTINF                  EXTINF
 	EXT_X_BYTERANGE         *EXT_X_BYTERANGE
 	EXT_X_DISCONTINUITY     bool
 	EXT_X_KEY               *EXT_X_KEY
@@ -15,21 +133,126 @@ type MediaSegment struct {
 }
 
 type MediaPlayList struct {
-	write                        Writer
+	writer                       Writer
 	EXT_X_VERSION                string
 	EXT_X_TARGETDURATION         string
 	EXT_X_MEDIA_SEQUENCE         string
 	EXT_X_DISCONTINUITY_SEQUENCE string
-	EXT_X_ENDLIST                bool
 	EXT_X_PLAYLIST_TYPE          string
 	EXT_X_I_FRAMES_ONLY          bool
 	EXT_X_INDEPENDENT_SEGMENTS   bool
 	EXT_X_START                  *EXT_X_START
 	MediaSegment                 []MediaSegment
+	EXT_X_ENDLIST                bool
+}
+
+func (mpl *MediaPlayList) Encode(w io.Writer) (err error) {
+	mpl.writer.SetWriter(w)
+	_, err = mpl.writer.EXTM3U()
+	if err != nil {
+		return
+	}
+	if mpl.EXT_X_VERSION != "" {
+		_, err = mpl.writer.EXT_X_VERSION(mpl.EXT_X_VERSION)
+		if err != nil {
+			return
+		}
+	}
+	if mpl.EXT_X_TARGETDURATION != "" {
+		_, err = mpl.writer.EXT_X_TARGETDURATION(mpl.EXT_X_TARGETDURATION)
+		if err != nil {
+			return
+		}
+	}
+	if mpl.EXT_X_MEDIA_SEQUENCE != "" {
+		_, err = mpl.writer.EXT_X_MEDIA_SEQUENCE(mpl.EXT_X_MEDIA_SEQUENCE)
+		if err != nil {
+			return
+		}
+	}
+	if mpl.EXT_X_DISCONTINUITY_SEQUENCE != "" {
+		_, err = mpl.writer.EXT_X_DISCONTINUITY_SEQUENCE(mpl.EXT_X_DISCONTINUITY_SEQUENCE)
+		if err != nil {
+			return
+		}
+	}
+	if mpl.EXT_X_PLAYLIST_TYPE != "" {
+		_, err = mpl.writer.EXT_X_PLAYLIST_TYPE(mpl.EXT_X_PLAYLIST_TYPE)
+		if err != nil {
+			return
+		}
+	}
+	if mpl.EXT_X_I_FRAMES_ONLY {
+		_, err = mpl.writer.EXT_X_I_FRAMES_ONLY()
+		if err != nil {
+			return
+		}
+	}
+	if mpl.EXT_X_INDEPENDENT_SEGMENTS {
+		_, err = mpl.writer.EXT_X_INDEPENDENT_SEGMENTS()
+		if err != nil {
+			return
+		}
+	}
+	if mpl.EXT_X_START != nil {
+		_, err = mpl.writer.EXT_X_START(mpl.EXT_X_START)
+		if err != nil {
+			return
+		}
+	}
+	for i := 0; i < len(mpl.MediaSegment); i++ {
+		_, err = mpl.writer.EXTINF(&mpl.MediaSegment[i].EXTINF)
+		if err != nil {
+			return
+		}
+		if mpl.MediaSegment[i].EXT_X_BYTERANGE != nil {
+			_, err = mpl.writer.EXT_X_BYTERANGE(mpl.MediaSegment[i].EXT_X_BYTERANGE)
+			if err != nil {
+				return
+			}
+		}
+		if mpl.MediaSegment[i].EXT_X_DISCONTINUITY {
+			_, err = mpl.writer.EXT_X_DISCONTINUITY()
+			if err != nil {
+				return
+			}
+		}
+		if mpl.MediaSegment[i].EXT_X_KEY != nil {
+			_, err = mpl.writer.EXT_X_KEY(mpl.MediaSegment[i].EXT_X_KEY)
+			if err != nil {
+				return
+			}
+		}
+		if mpl.MediaSegment[i].EXT_X_MAP != nil {
+			_, err = mpl.writer.EXT_X_MAP(mpl.MediaSegment[i].EXT_X_MAP)
+			if err != nil {
+				return
+			}
+		}
+		if mpl.MediaSegment[i].EXT_X_PROGRAM_DATE_TIME != "" {
+			_, err = mpl.writer.EXT_X_PROGRAM_DATE_TIME(mpl.MediaSegment[i].EXT_X_PROGRAM_DATE_TIME)
+			if err != nil {
+				return
+			}
+		}
+		if mpl.MediaSegment[i].EXT_X_DATERANGE != nil {
+			_, err = mpl.writer.EXT_X_DATERANGE(mpl.MediaSegment[i].EXT_X_DATERANGE)
+			if err != nil {
+				return
+			}
+		}
+	}
+	if mpl.EXT_X_ENDLIST {
+		_, err = mpl.writer.EXT_X_ENDLIST()
+		if err != nil {
+			return
+		}
+	}
+	return
 }
 
 type MasterPlayList struct {
-	write                      Writer
+	writer                     Writer
 	EXT_X_VERSION              string
 	EXT_X_MEDIA                []EXT_X_MEDIA
 	EXT_X_STREAM_INF           []EXT_X_STREAM_INF
@@ -38,4 +261,61 @@ type MasterPlayList struct {
 	EXT_X_SESSION_KEY          *EXT_X_KEY
 	EXT_X_INDEPENDENT_SEGMENTS bool
 	EXT_X_START                *EXT_X_START
+}
+
+func (mpl *MasterPlayList) Encode(w io.Writer) (err error) {
+	mpl.writer.SetWriter(w)
+	_, err = mpl.writer.EXTM3U()
+	if err != nil {
+		return
+	}
+	if mpl.EXT_X_VERSION != "" {
+		_, err = mpl.writer.EXT_X_VERSION(mpl.EXT_X_VERSION)
+		if err != nil {
+			return
+		}
+	}
+	for i := 0; i < len(mpl.EXT_X_MEDIA); i++ {
+		_, err = mpl.writer.EXT_X_MEDIA(&mpl.EXT_X_MEDIA[i])
+		if err != nil {
+			return
+		}
+	}
+	for i := 0; i < len(mpl.EXT_X_STREAM_INF); i++ {
+		_, err = mpl.writer.EXT_X_STREAM_INF(&mpl.EXT_X_STREAM_INF[i])
+		if err != nil {
+			return
+		}
+	}
+	for i := 0; i < len(mpl.EXT_X_I_FRAME_STREAM_INF); i++ {
+		_, err = mpl.writer.EXT_X_I_FRAME_STREAM_INF(&mpl.EXT_X_I_FRAME_STREAM_INF[i])
+		if err != nil {
+			return
+		}
+	}
+	for i := 0; i < len(mpl.EXT_X_SESSION_DATA); i++ {
+		_, err = mpl.writer.EXT_X_SESSION_DATA(&mpl.EXT_X_SESSION_DATA[i])
+		if err != nil {
+			return
+		}
+	}
+	if mpl.EXT_X_SESSION_KEY != nil {
+		_, err = mpl.writer.EXT_X_SESSION_KEY(mpl.EXT_X_SESSION_KEY)
+		if err != nil {
+			return
+		}
+	}
+	if mpl.EXT_X_INDEPENDENT_SEGMENTS {
+		_, err = mpl.writer.EXT_X_INDEPENDENT_SEGMENTS()
+		if err != nil {
+			return
+		}
+	}
+	if mpl.EXT_X_START != nil {
+		_, err = mpl.writer.EXT_X_START(mpl.EXT_X_START)
+		if err != nil {
+			return
+		}
+	}
+	return
 }
--- writer.go
+++ writer.go
@@ -3,28 +3,37 @@ package m3u8
 import "io"
 
 type Writer struct {
-	buff   []byte
-	writer io.Writer
+	buff   []byte    // 缓存
+	writer io.Writer // 输出目标
 }
 
+// writer是接收输出的数据
 func NewWriter(writer io.Writer) *Writer {
 	p := new(Writer)
 	p.writer = writer
 	return p
 }
 
+// 设置writer
+func (w *Writer) SetWriter(writer io.Writer) {
+	w.writer = writer
+}
+
+// 刷新数据，buff写到writer中
 func (w *Writer) flushBuffer() (int, error) {
 	n, err := w.writer.Write(w.buff)
 	w.buff = w.buff[:0]
 	return n, err
 }
 
+// s有数据才添加到缓存buff
 func (w *Writer) writeNoEmptyString(s string) {
 	if s != "" {
 		w.buff = append(w.buff, s...)
 	}
 }
 
+// s有数据才添加到缓存buff，先添加a，在添加s
 func (w *Writer) writeAttributeNoEmptyString(a, s string) {
 	if s != "" {
 		w.buff = append(w.buff, a...)
@@ -32,14 +41,15 @@ func (w *Writer) writeAttributeNoEmptyString(a, s string) {
 	}
 }
 
-func (w *Writer) TagEXTM3U() (int, error) {
-	w.buff = w.buff[:0]
+// #EXTM3U
+func (w *Writer) EXTM3U() (int, error) {
 	w.buff = append(w.buff, "#EXTM3U\n"...)
 	// 输出
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_VERSION(tag string) (int, error) {
+// #EXT-X-VERSION:<n>
+func (w *Writer) EXT_X_VERSION(tag string) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-VERSION:"...)
 	w.buff = append(w.buff, tag...)
 	w.buff = append(w.buff, '\n')
@@ -47,7 +57,8 @@ func (w *Writer) TagEXT_X_VERSION(tag string) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXTINF(tag *EXTINF) (int, error) {
+// #EXTINF:<duration>,[<title>]
+func (w *Writer) EXTINF(tag *EXTINF) (int, error) {
 	w.buff = append(w.buff, "#EXTINF:"...)
 	// duration
 	w.buff = append(w.buff, tag.DURATION...)
@@ -60,7 +71,8 @@ func (w *Writer) TagEXTINF(tag *EXTINF) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_BYTERANGE(tag *EXT_X_BYTERANGE) (int, error) {
+// #EXT-X-BYTERANGE:<n>[@<o>]
+func (w *Writer) EXT_X_BYTERANGE(tag *EXT_X_BYTERANGE) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-BYTERANGE:"...)
 	// n
 	w.buff = append(w.buff, tag.N...)
@@ -75,13 +87,15 @@ func (w *Writer) TagEXT_X_BYTERANGE(tag *EXT_X_BYTERANGE) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_DISCONTINUITY() (int, error) {
+// #EXT-X-DISCONTINUITY
+func (w *Writer) EXT_X_DISCONTINUITY() (int, error) {
 	w.buff = append(w.buff, "#EXT-X-DISCONTINUITY\n"...)
 	// 输出
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_KEY(tag *EXT_X_KEY) (int, error) {
+// #EXT-X-KEY:<attribute-list>
+func (w *Writer) EXT_X_KEY(tag *EXT_X_KEY) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-KEY:"...)
 	// METHOD
 	w.writeAttributeNoEmptyString("METHOD=", tag.METHOD)
@@ -99,7 +113,8 @@ func (w *Writer) TagEXT_X_KEY(tag *EXT_X_KEY) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_MAP(tag *EXT_X_MAP) (int, error) {
+// #EXT-X-MAP:<attribute-list>
+func (w *Writer) EXT_X_MAP(tag *EXT_X_MAP) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-MAP:"...)
 	// URI
 	w.writeAttributeNoEmptyString("URI=", tag.URI)
@@ -111,7 +126,8 @@ func (w *Writer) TagEXT_X_MAP(tag *EXT_X_MAP) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_PROGRAM_DATE_TIME(dateTime string) (int, error) {
+// #EXT-X-PROGRAM-DATE-TIME:<date-time-msec>
+func (w *Writer) EXT_X_PROGRAM_DATE_TIME(dateTime string) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-PROGRAM-DATE-TIME:"...)
 	w.buff = append(w.buff, dateTime...)
 	// 换行
@@ -120,7 +136,8 @@ func (w *Writer) TagEXT_X_PROGRAM_DATE_TIME(dateTime string) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_DATERANGE(tag *EXT_X_DATERANGE) (int, error) {
+// #EXT-X-DATERANGE:<attribute-list>
+func (w *Writer) EXT_X_DATERANGE(tag *EXT_X_DATERANGE) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-DATERANGE:"...)
 	// ID
 	w.writeAttributeNoEmptyString("ID=", tag.ID)
@@ -154,7 +171,8 @@ func (w *Writer) TagEXT_X_DATERANGE(tag *EXT_X_DATERANGE) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_TARGETDURATION(tag string) (int, error) {
+// #EXT-X-TARGETDURATION:<s>
+func (w *Writer) EXT_X_TARGETDURATION(tag string) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-TARGETDURATION:"...)
 	w.buff = append(w.buff, tag...)
 	w.buff = append(w.buff, '\n')
@@ -162,7 +180,8 @@ func (w *Writer) TagEXT_X_TARGETDURATION(tag string) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_MEDIA_SEQUENCE(tag string) (int, error) {
+// #EXT-X-MEDIA-SEQUENCE:<s>
+func (w *Writer) EXT_X_MEDIA_SEQUENCE(tag string) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-MEDIA-SEQUENCE:"...)
 	w.buff = append(w.buff, tag...)
 	w.buff = append(w.buff, '\n')
@@ -170,7 +189,8 @@ func (w *Writer) TagEXT_X_MEDIA_SEQUENCE(tag string) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_DISCONTINUITY_SEQUENCE(tag string) (int, error) {
+// #EXT-X-DISCONTINUITY-SEQUENCE:<number>
+func (w *Writer) EXT_X_DISCONTINUITY_SEQUENCE(tag string) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-DISCONTINUITY-SEQUENCE:"...)
 	w.buff = append(w.buff, tag...)
 	w.buff = append(w.buff, '\n')
@@ -178,13 +198,15 @@ func (w *Writer) TagEXT_X_DISCONTINUITY_SEQUENCE(tag string) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_ENDLIST(tag string) (int, error) {
+// #EXT-X-ENDLIST
+func (w *Writer) EXT_X_ENDLIST() (int, error) {
 	w.buff = append(w.buff, "#EXT-X-ENDLIST\n"...)
 	// 输出
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_PLAYLIST_TYPE(tag string) (int, error) {
+// #EXT-X-PLAYLIST-TYPE:<type-enum>
+func (w *Writer) EXT_X_PLAYLIST_TYPE(tag string) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-PLAYLIST-TYPE:"...)
 	w.buff = append(w.buff, tag...)
 	w.buff = append(w.buff, '\n')
@@ -192,13 +214,15 @@ func (w *Writer) TagEXT_X_PLAYLIST_TYPE(tag string) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_I_FRAMES_ONLY(tag string) (int, error) {
+// #EXT-X-I-FRAMES-ONLY
+func (w *Writer) EXT_X_I_FRAMES_ONLY() (int, error) {
 	w.buff = append(w.buff, "#EXT-X-I-FRAMES-ONLY\n"...)
 	// 输出
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_MEDIA(tag *EXT_X_MEDIA) (int, error) {
+// #EXT-X-MEDIA:<attribute-list>
+func (w *Writer) EXT_X_MEDIA(tag *EXT_X_MEDIA) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-MEDIA:"...)
 	// TYPE
 	w.writeAttributeNoEmptyString("TYPE=", tag.TYPE)
@@ -230,7 +254,9 @@ func (w *Writer) TagEXT_X_MEDIA(tag *EXT_X_MEDIA) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_STREAM_INF(tag *EXT_X_STREAM_INF) (int, error) {
+// #EXT-X-STREAM-INF:<attribute-list>
+// <URI>
+func (w *Writer) EXT_X_STREAM_INF(tag *EXT_X_STREAM_INF) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-STREAM-INF:"...)
 	// BANDWIDTH
 	w.writeAttributeNoEmptyString("BANDWIDTH=", tag.BANDWIDTH)
@@ -261,7 +287,9 @@ func (w *Writer) TagEXT_X_STREAM_INF(tag *EXT_X_STREAM_INF) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_I_FRAME_STREAM_INF(tag *EXT_X_I_FRAME_STREAM_INF) (int, error) {
+// #EXT-X-I-FRAME-STREAM-INF:<attribute-list>
+// <URI>
+func (w *Writer) EXT_X_I_FRAME_STREAM_INF(tag *EXT_X_I_FRAME_STREAM_INF) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-I-FRAME-STREAM-INF:"...)
 	// BANDWIDTH
 	w.writeAttributeNoEmptyString("BANDWIDTH=", tag.BANDWIDTH)
@@ -284,7 +312,8 @@ func (w *Writer) TagEXT_X_I_FRAME_STREAM_INF(tag *EXT_X_I_FRAME_STREAM_INF) (int
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_SESSION_DATA(tag *EXT_X_SESSION_DATA) (int, error) {
+// #EXT-X-SESSION-DATA:<attribute-list>
+func (w *Writer) EXT_X_SESSION_DATA(tag *EXT_X_SESSION_DATA) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-SESSION-DATA:"...)
 	// DATA_ID
 	w.writeAttributeNoEmptyString("DATA_ID=", tag.DATA_ID)
@@ -300,7 +329,8 @@ func (w *Writer) TagEXT_X_SESSION_DATA(tag *EXT_X_SESSION_DATA) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_SESSION_KEY(tag *EXT_X_KEY) (int, error) {
+// #EXT-X-SESSION-KEY:<attribute-list>
+func (w *Writer) EXT_X_SESSION_KEY(tag *EXT_X_KEY) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-SESSION-KEY:"...)
 	// METHOD
 	w.writeAttributeNoEmptyString("METHOD=", tag.METHOD)
@@ -318,13 +348,15 @@ func (w *Writer) TagEXT_X_SESSION_KEY(tag *EXT_X_KEY) (int, error) {
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_I_INDEPENDENT_SEGMENTS(tag string) (int, error) {
-	w.buff = append(w.buff, "#EXT-X-I-INDEPENDENT-SEGMENTS\n"...)
+// #EXT-X-INDEPENDENT-SEGMENTS
+func (w *Writer) EXT_X_INDEPENDENT_SEGMENTS() (int, error) {
+	w.buff = append(w.buff, "#EXT-X-INDEPENDENT-SEGMENTS\n"...)
 	// 输出
 	return w.flushBuffer()
 }
 
-func (w *Writer) TagEXT_X_START(tag *EXT_X_START) (int, error) {
+// #EXT-X-START:<attribute-list>
+func (w *Writer) EXT_X_START(tag *EXT_X_START) (int, error) {
 	w.buff = append(w.buff, "#EXT-X-START:"...)
 	// TIME_OFFSET
 	w.writeAttributeNoEmptyString("TIME_OFFSET=", tag.TIME_OFFSET)
